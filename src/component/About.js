af2fc2cddfc05ba2fdab3ee424605ef65028d33a
Rohit Singh <kumar.rohitsingh37@gmail.com>
2024-10-09 17:18:03 +0530

    Rohit Singh

--- src/component/About.js
+++ src/component/About.js
@@ -4,32 +4,34 @@ function About(props) {
     return (
         <>
             <div id="about" >
-                <div className="about" style={{color: (props.mode == 'dark'? 'rgba(250, 236, 236, 0.997)': 'rgba(0, 0, 0, 1)'), background: (props.mode == 'dark' ? 'rgba(0, 0, 0, 1)': 'rgba(255, 255, 255, 1)')}}>
+                <div className="about" style={{ color: (props.mode == 'dark' ? 'rgba(250, 236, 236, 0.997)' : 'rgba(0, 0, 0, 1)'), background: (props.mode == 'dark' ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)') }}>
                     <p>
-                        <span>
+                        <h4>
                             welcome to my web page. :)
-                        </span>
+                        </h4>
                     </p>
                     <br />
                     <p>
                         <span>
-                            My name is Rohit Singh, and I am preparing for software engineer.
+                        Hi! I’m Rohit Singh, an aspiring software engineer pursuing a BCA at Sri Dev Suman Uttarakhand University.
                         </span>
                     </p>
                     <p>
                         <span>
-                            I am pursuing BCA from sri dev suman Uttarakhand university. My hobby is playing cricket,
-                            football and many
-                            physical game, I also love solving puzzles and learning about the world. and i enjoy learning
-                            new things.
+                        I’m passionate about technology and sports, enjoying cricket, football, and various games. I also love solving puzzles and learning about the world. Continuous learning is important to me, and I’m always eager to expand my skills.
+                        </span>
+                    </p>
+                    <p>
+                        <span>
+                            Thanks for visiting my page!
                         </span>
                     </p>
 
                 </div>
                 <img src="photo/rohit2.JPG" alt="" className="rohit"></img>
             </div>
-            <div className="about_me" style={{color: (props.mode == 'dark'? 'antiquewhite': 'rgba(0, 0, 0, 1)'), background: (props.mode == 'dark' ? 'rgba(0, 0, 0, 1)': 'rgba(255, 255, 255, 1)')}}>
-                <div className="about1"  style={{color: (props.mode == 'dark'? 'rgb(231, 172, 93)' : 'rgb(181, 103, 0)')}}>
+            <div className="about_me" style={{ color: (props.mode == 'dark' ? 'antiquewhite' : 'rgba(0, 0, 0, 1)'), background: (props.mode == 'dark' ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)') }}>
+                <div className="about1" style={{ color: (props.mode == 'dark' ? 'rgb(231, 172, 93)' : 'rgb(181, 103, 0)') }}>
                     <div className="detail1">Name:</div>
                     <div className="detail1">S/O:</div>
                     <div className="detail1">DOB:</div>
